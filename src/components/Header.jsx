7cd78d9357e3ad4a5b68c0b8c85da97244a074b7
Shivam Sehgal <coderrshivam@gmail.com>
2025-07-10 00:15:47 +0530

    Fixing image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth";
@@ -6,8 +6,11 @@ import { LOGO } from "../utils/constant";
 import { addUser, removeUser } from "../store/userSlice";
 import { toggleGptSearchView } from "../store/gptSlice";
 import { auth } from "../firebase/firebase";
+import { Search } from "lucide-react";
 
 const Header = () => {
+  const [displaySearch, setdisplaySearch] = useState(true);
+
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -29,6 +32,7 @@ const Header = () => {
   };
 
   const handleGptSearchClick = () => {
+    setdisplaySearch(false);
     dispatch(toggleGptSearchView());
   };
 
@@ -61,18 +65,25 @@ const Header = () => {
     <div className="absolute w-full px-0 md:p-0 md:pr-8 md:py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
       <img
         onClick={navigateHome}
-        className="w-30 md:w-80 h-20"
+        className="w-35 h-20 md:w-80 md:h-20"
         src={LOGO}
         alt="logo"
       />
       {user && (
         <div className="flex items-center m-2">
-          <button
-            onClick={handleGptSearchClick}
-            className="bg-purple-800 py-2 px-3 md:py-2 md:px-4 rounded-md mx-4 text-sm md:text-lg text-white cursor-pointer"
-          >
-            GPT Search
-          </button>
+          {displaySearch && (
+            <div
+              onClick={handleGptSearchClick}
+              className="w-40 flex items-center gap-2 p-2 border py-2 md:py-2 md:px-4 rounded-md mx-2 text-sm md:text-lg text-white cursor-pointe"
+            >
+              <Search className="text-gray-600" />
+              <input
+                type="text"
+                placeholder="AI Search"
+                className="outline-none w-full"
+              />
+            </div>
+          )}
 
           <img
             className="w-8 md:w-12 rounded-md md:rounded-3xl"
